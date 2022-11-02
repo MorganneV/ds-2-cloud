be44d5dabcb205528a0388ed8b1f38f88bbf47d1
MorganneV <49565443+MorganneV@users.noreply.github.com>
2022-11-02 14:43:35 +0100

    Tried getting the flights to the website

---
+++ src/main/java/be/kuleuven/distributedsystems/cloud/Model.java
@@ -0,0 +1,100 @@
+package be.kuleuven.distributedsystems.cloud;
+
+import be.kuleuven.distributedsystems.cloud.entities.Booking;
+import be.kuleuven.distributedsystems.cloud.entities.Flight;
+import be.kuleuven.distributedsystems.cloud.entities.Seat;
+import be.kuleuven.distributedsystems.cloud.entities.Ticket;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.annotation.Resource;
+import java.awt.print.Book;
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class Model {
+
+    @Resource(name = "webClientBuilder")
+    WebClient.Builder webClientBuilder;
+
+    public String API_KEY = "Iw8zeveVyaPNWonPNaU0213uw3g6Ei";
+
+    public String reliableAirline = "https://reliable-airline.com";
+    public String unreliableAirline = "https://unreliable-airline.com";
+    public List<String> airlines = Arrays.asList(reliableAirline);
+
+    public List<Flight> getFlights(){
+        List<Flight> flights = new ArrayList<>();
+        for (String airline: airlines) {
+            flights.addAll(webClientBuilder
+                            .baseUrl("https://reliable-airline.com")
+                            .build()
+                            .get()
+                            .uri(uriBuilder -> uriBuilder.pathSegment("flights").queryParam("key", API_KEY).build())
+                            .retrieve()
+                            .bodyToMono(new ParameterizedTypeReference<CollectionModel<Flight>>() {})
+                            .block()
+                            .getContent());
+
+        }
+        return flights;
+    }
+
+    public Flight getFlight(String airline, UUID flightId){
+        List<Flight> flights = getFlights();
+        for (Flight flight: flights) {
+            if(flight.getAirline().equals(airline) && flight.getFlightId().equals(flightId)){
+                return flight;
+            }
+        }
+        return null;
+    }
+
+    //TODO
+    public List<LocalDateTime> getFlightTimes(String airline, UUID flightId){
+        return null;
+    }
+
+    //TODO
+    public List<Seat> getAvailableSeats(String airline, UUID flightId, LocalDateTime time){
+        return null;
+    }
+
+    //todo
+    public Seat getSeat(String airline, UUID flightId, UUID seatId){
+        return null;
+    }
+
+    //todo
+    public void confirmQuotes(){}
+
+    //todo
+    public Ticket getTicket(String airline, UUID flightId, UUID seatId){
+        return null;
+    }
+
+    //todo
+    public List<Booking> getBookings(String user){
+        return null;
+    }
+
+    //todo
+    public List<Booking> getAllBookings(){
+        return null;
+    }
+
+    //todo
+    public List<String> getBestCustomers(){
+        return null;
+    }
+
+
+
+}
--- src/main/java/be/kuleuven/distributedsystems/cloud/controller/ViewController.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/controller/ViewController.java
@@ -1,17 +1,68 @@
 package be.kuleuven.distributedsystems.cloud.controller;
 
+import be.kuleuven.distributedsystems.cloud.Model;
+import be.kuleuven.distributedsystems.cloud.entities.Flight;
+import be.kuleuven.distributedsystems.cloud.entities.Quote;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.resource.CachingResourceTransformer;
 
-@Controller
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
 public class ViewController {
+    private final Model model;
 
-    @GetMapping({"/flights/*/*", "/flights/*/*/*", "/cart", "/account", "/manager", "/login"})
-    public String spa() {
-        return "forward:/";
+    @Autowired
+    public ViewController(Model model){
+        this.model = model;
     }
+
+//    @GetMapping({"/flights/*/*", "/flights/*/*/*", "/cart", "/account", "/manager", "/login"})
+//    public String spa() {
+//        return "forward:/";
+//    }
 
     @GetMapping("/_ah/warmup")
     public void warmup() {
     }
+
+    @GetMapping({"/","/flights"})
+    public ModelAndView getFlights() {
+        ModelAndView modelAndView = new ModelAndView("flights");
+        modelAndView.addObject("flights", this.model.getFlights());
+        return modelAndView;
+    }
+
+//
+//    @GetMapping("flights/{airline}/{flightId}")
+//    public ModelAndView getFlightTimes(@PathVariable String airline, @PathVariable UUID flightId){
+//        ModelAndView modelAndView = new ModelAndView("flight_times");
+//        modelAndView.addObject("flight", this.model.getFlight(airline, flightId));
+//        modelAndView.addObject("flightTimes", this.model.getFlightTimes(airline,flightId).stream().sorted().collect(Collectors.toList()));
+//        return modelAndView;
+//    }
+//
+//    @GetMapping("flights/{airline}/{flightId}/{time}")
+//    public ModelAndView getFlightSeats(@PathVariable String airline, @PathVariable UUID flightId, @PathVariable LocalDateTime time){
+//        ModelAndView modelAndView = new ModelAndView("flight_seats");
+//        modelAndView.addObject("flight", this.model.getFlight(airline, flightId));
+//        modelAndView.addObject("time", time.format(DateTimeFormatter.ofPattern("d MMM uuuu H:mm")));
+//
+//        return ModelAndView;
+//    }
+
+
+//
+//    )
 }
