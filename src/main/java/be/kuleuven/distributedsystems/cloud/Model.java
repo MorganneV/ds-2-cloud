f4c4c2ed6cf9da0333c80097664f60103610b352
MorganneV <49565443+MorganneV@users.noreply.github.com>
2022-11-08 12:38:27 +0100

    Model.java aangepast

--- src/main/java/be/kuleuven/distributedsystems/cloud/Model.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/Model.java
@@ -7,7 +7,10 @@ import be.kuleuven.distributedsystems.cloud.entities.Ticket;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -24,7 +27,7 @@ import java.util.*;
 public class Model {
 
     @Resource(name = "webClientBuilder")
-    private WebClient.Builder webClientBuilder;
+    WebClient.Builder webClientBuilder;
 
     public String API_KEY = "Iw8zeveVyaPNWonPNaU0213uw3g6Ei";
 
@@ -34,11 +37,11 @@ public class Model {
 
     @Retryable
     @GetMapping("/getFlights")
-    public Collection<Flight> getFlights(){
-        Collection<Flight> flights = new ArrayList<>();
+    public List<Flight> getFlights(){
+        List<Flight> flights = new ArrayList<>();
         for (String airline: airlines) {
             flights.addAll(webClientBuilder
-                            .baseUrl("https://reliable-airline.com")
+                            .baseUrl(airline)
                             .build()
                             .get()
                             .uri(uriBuilder -> uriBuilder.pathSegment("flights").queryParam("key", API_KEY).build())
@@ -52,37 +55,35 @@ public class Model {
     }
 
     @GetMapping(("/getFlight/{airline}/{flightId}"))
-    public Flight getFlight(String airline, UUID flightId) {
-        String id = flightId.toString();
+    public Flight getFlight(@PathVariable String airline, @PathVariable UUID flightId){
         Flight flight = webClientBuilder
                 .baseUrl("https://" + airline)
                 .build()
                 .get()
                 .uri(uriBuilder -> uriBuilder
-                        .pathSegment("flights/" + id)
+                        .pathSegment("flights/" + flightId.toString())
                         .queryParam("key", API_KEY)
                         .build())
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<Flight>() {
-                })
+                .bodyToMono(new ParameterizedTypeReference<Flight>() {})
                 .block();
         return flight;
     }
 
     //TODO
     @GetMapping(("/getFlightTimes/{airline}/{flightId}"))
-    public Collection<LocalDateTime> getFlightTimes(String airline, UUID flightId){
+    public Collection<LocalDateTime> getFlightTimes(@PathVariable String airline, @PathVariable UUID flightId){
         Flight flight = getFlight(airline, flightId);
         Collection<LocalDateTime> result = new ArrayList<>();
         String id = flight.getFlightId().toString();
         var times = webClientBuilder
-                .baseUrl("https://reliable-airline.com")
-                .build()
-                .get()
-                .uri(uriBuilder -> uriBuilder.pathSegment("flights/" + id + "/times/").queryParam("key", API_KEY).build())
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<CollectionModel<String>>() {})
-                .block();
+                .baseUrl("https://reliable-airline.com/")
+                        .build()
+                        .get()
+                        .uri(uriBuilder -> uriBuilder.pathSegment("flights/" + id + "/times/").queryParam("key", API_KEY).build())
+                        .retrieve()
+                        .bodyToMono(new ParameterizedTypeReference<CollectionModel<String>>() {})
+                        .block();
         if (times != null) {
             for (String t : times) {
                 result.add(LocalDateTime.parse(t, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
@@ -90,16 +91,47 @@ public class Model {
             return result;
         }
         return null;
-        }
+    }
 
     //TODO
-    public List<Seat> getAvailableSeats(String airline, UUID flightId, LocalDateTime time){
-        return null;
+    @GetMapping("/getAvailableSeats/{airline}/{flightId}/{time}")
+    public List<Seat> getAvailableSeats(@PathVariable String airline, @PathVariable UUID flightId, @PathVariable LocalDateTime time){
+        List<Seat> seats = new ArrayList<>();
+        String id = flightId.toString();
+        String path = "/flights/" + id + "/seats";
+        MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
+        params.add("time", time.toString());
+        params.add("available","true");
+        params.add("key", API_KEY);
+        seats.addAll(webClientBuilder
+                        .baseUrl("https://" + airline)
+                        .build()
+                        .get()
+                        .uri(uriBuilder -> uriBuilder.pathSegment(path).queryParams(params).build())
+                        .retrieve()
+                        .bodyToMono(new ParameterizedTypeReference<CollectionModel<Seat>>() {})
+                        .block()
+                        .getContent());
+        return seats;
     }
 
     //todo
-    public Seat getSeat(String airline, UUID flightId, UUID seatId){
-        return null;
+    @GetMapping("/getSeat/{airline}/{flightId}/{seatId}")
+    public Seat getSeat(@PathVariable String airline, @PathVariable UUID flightId, @PathVariable UUID seatId){
+        String path = "flights/" + flightId.toString() + "/seat/" + seatId.toString();
+        Seat seat = webClientBuilder
+                .baseUrl("https://" + airline)
+                .build()
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .pathSegment(path)
+                        .queryParam("key", API_KEY)
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Seat>() {
+                })
+                .block();
+        return seat;
     }
 
     //todo
