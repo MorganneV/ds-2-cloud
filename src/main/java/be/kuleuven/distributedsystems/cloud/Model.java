03c7ad52ba85fc5fda68c510349cf55cb4314f82
MorganneV <49565443+MorganneV@users.noreply.github.com>
2022-11-16 17:42:39 +0100

    getBestCustomer werkt

--- src/main/java/be/kuleuven/distributedsystems/cloud/Model.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/Model.java
@@ -40,8 +40,8 @@ public class Model {
 
     public String API_KEY = "Iw8zeveVyaPNWonPNaU0213uw3g6Ei";
 
-    public String reliableAirline = "https://reliable-airline.com";
-    public String unreliableAirline = "https://unreliable-airline.com";
+    public String reliableAirline = "reliable-airline.com";
+    public String unreliableAirline = "unreliable-airline.com";
     public List<String> airlines = Arrays.asList(reliableAirline);
 
     @Retryable
@@ -50,19 +50,20 @@ public class Model {
         List<Flight> flights = new ArrayList<>();
         for (String airline: airlines) {
             flights.addAll(webClientBuilder
-                    .baseUrl("https://reliable-airline.com")
-                    .build()
-                    .get()
-                    .uri(uriBuilder -> uriBuilder.pathSegment("flights").queryParam("key", API_KEY).build())
-                    .retrieve()
-                    .bodyToMono(new ParameterizedTypeReference<CollectionModel<Flight>>() {})
-                    .block()
-                    .getContent());
+                            .baseUrl("https://" + airline)
+                            .build()
+                            .get()
+                            .uri(uriBuilder -> uriBuilder.pathSegment("flights").queryParam("key", API_KEY).build())
+                            .retrieve()
+                            .bodyToMono(new ParameterizedTypeReference<CollectionModel<Flight>>() {})
+                            .block()
+                            .getContent());
 
         }
         return flights;
     }
 
+    @Retryable
     @GetMapping(("/getFlight"))
     public Flight getFlight(@RequestParam("airline") String airline, @RequestParam("flightId") UUID flightId){
         Flight flight = webClientBuilder
@@ -80,6 +81,7 @@ public class Model {
         return flight;
     }
 
+    @Retryable
     @GetMapping(("/getFlightTimes"))
     public List<LocalDateTime> getFlightTimes(@RequestParam("airline") String airline, @RequestParam("flightId") UUID flightId){
         List<LocalDateTime> times = new ArrayList<>();
@@ -101,26 +103,27 @@ public class Model {
         return times;
     }
 
+    @Retryable
     @GetMapping("/getAvailableSeats")
     public Map<String,List<Seat>> getAvailableSeats(@RequestParam("airline") String airline, @RequestParam("flightId") UUID flightId, @RequestParam("time") String time){
         List<Seat> seats = new ArrayList<>();
         String id = flightId.toString();
         seats.addAll(webClientBuilder
-                .baseUrl("https://" + airline )
-                .build()
-                .get()
-                .uri(uriBuilder -> uriBuilder
-                        .pathSegment("flights")
-                        .pathSegment(id)
-                        .pathSegment("seats")
-                        .queryParam("time", time)
-                        .queryParam("available", "true")
-                        .queryParam("key", API_KEY)
-                        .build())
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<CollectionModel<Seat>>() {})
-                .block()
-                .getContent());
+                        .baseUrl("https://" + airline )
+                        .build()
+                        .get()
+                        .uri(uriBuilder -> uriBuilder
+                                .pathSegment("flights")
+                                .pathSegment(id)
+                                .pathSegment("seats")
+                                .queryParam("time", time)
+                                .queryParam("available", "true")
+                                .queryParam("key", API_KEY)
+                                .build())
+                        .retrieve()
+                        .bodyToMono(new ParameterizedTypeReference<CollectionModel<Seat>>() {})
+                        .block()
+                        .getContent());
         Map<String, List<Seat>> availableSeats = new HashMap<String, List<Seat>>();
         for (Seat seat: seats) {
             if (!availableSeats.containsKey(seat.getType())){
@@ -132,6 +135,7 @@ public class Model {
     }
 
 
+    @Retryable
     @GetMapping("/getSeat")
     public Seat getSeat(@RequestParam("airline") String airline, @RequestParam("flightId") UUID flightId, @RequestParam("seatId") UUID seatId){
         Seat seat = webClientBuilder
@@ -153,7 +157,8 @@ public class Model {
     }
 
     private WebSecurityConfig webSecurityConfig = new WebSecurityConfig(new SecurityFilter());
-    //fixme
+
+    @Retryable(maxAttempts = 4)
     @PostMapping({"/confirmQuotes"})
     public void confirmQuotes(@RequestBody List<Quote> quotes) {
         String customer = webSecurityConfig.getUser().getEmail();
@@ -189,8 +194,9 @@ public class Model {
             }
             Booking booking = new Booking(UUID.randomUUID(), LocalDateTime.now(), reservedTickets, customer);
             reservedBookings.add(booking);
-            reservedTickets.clear();
-            handledQuotes.clear();
+
+            reservedTickets = new ArrayList<>();
+            handledQuotes = new ArrayList<>();
         } catch(IllegalStateException e) {
             e.printStackTrace();
             for (Ticket ticket: reservedTickets) {
@@ -224,7 +230,7 @@ public class Model {
     }
 
 
-
+    @Retryable
     private Ticket getTicket(String airline, UUID flightId, UUID seatId){
         Ticket ticket = webClientBuilder
                 .baseUrl("https://" + airline)
@@ -245,7 +251,7 @@ public class Model {
         return ticket;
     }
 
-    //todo
+
     @GetMapping("/getBookings")
     public List<Booking> getBookings(){
         String user = webSecurityConfig.getUser().getEmail();
@@ -258,35 +264,38 @@ public class Model {
     }
 
 
-    //todo
     @GetMapping("/getAllBookings")
     public List<Booking> getAllBookings(){
         return getReservedBookings();
     }
 
-    //todo
+
     @GetMapping("/getBestCustomers")
     public Set<String> getBestCustomers(){
-        Map<String, Integer> topCustomers = new HashMap<>();
-        for (Booking booking: getReservedBookings()) {
-            if (!topCustomers.containsKey(booking.getCustomer())){
-                topCustomers.put(booking.getCustomer(), 0);
+        Map<String, Integer> customers = new HashMap<>();
+        for (Booking booking: getReservedBookings()){
+            String customer = booking.getCustomer();
+            if(!customers.containsKey(customer)){
+                customers.put(customer, 0);
             }
-            topCustomers.put(booking.getCustomer(), topCustomers.get(booking.getCustomer()) + booking.getTickets().size());
+            Integer ticketsAmount = booking.getTickets().size();
+            Integer newAmount = customers.get(customer) + ticketsAmount;
+            customers.put(customer, newAmount);
         }
 
         Set<String> bestCustomer = new HashSet<>();
-        Integer amount = 0;
-        for (String customer : topCustomers.keySet()) {
-            Integer nbTickets = topCustomers.get(customer);
-            if (nbTickets > amount){
-//                amount = nbTickets;
+        Integer best = 0;
+        for (String c: customers.keySet()) {
+            Integer nbTickets = customers.get(c);
+            if (nbTickets > best){
                 bestCustomer.clear();
-                bestCustomer.add(customer);
-            } else if (nbTickets.equals(amount)){
-                bestCustomer.add(customer);
+                bestCustomer.add(c);
+                best = nbTickets;
+            } else if (nbTickets.equals(best)){
+                bestCustomer.add(c);
             }
         }
+
         return bestCustomer;
     }
 
